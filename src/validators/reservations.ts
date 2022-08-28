944a9cfb2e78111b7e7f87a9d7fb1eb21d344eca
Ayomikun Emmanuel <adeolaayo31@yahoo.com>
2022-08-28 22:01:55 +0100

    chore(validators): modified reservation info validator

--- src/validators/reservations.ts
+++ src/validators/reservations.ts
@@ -1,6 +1,12 @@
 import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+import baseValidator from './index';
 
-export const calculateOverStayFeeValidation: any = Joi.object({
-    customer_id: Joi.string().required(),
+
+export const calculateOverStayFeeValidator = async (req: Request, res: Response, next: NextFunction) => {
+  const schema = Joi.object({
+    reservation_id: Joi.string().required(),
     actual_checkout_time: Joi.string().required(),
-})
+  });
+  await baseValidator(schema, req, res, next, 'body');
+};
